49b65871a7184b841425e7d5d9d6f0cd4b1ff014
Fran <etchartfran1997@gmail.com>
2023-01-09 14:13:16 -0300

    actualizacion

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { AuthCrecencialesDto } from './dto/auth.credenciales.dto';
-
+import { AuthService } from './auth.service';
 @Controller('auth')
 export class AuthController {
+  constructor(private authService: AuthService) {}
   @Post('/registro')
-  registro(@Body() authCredencialesDto: AuthCrecencialesDto) {}
+  registro(
+    @Body(ValidationPipe) authCredencialesDto: AuthCrecencialesDto,
+  ): Promise<void> {
+    return this.authService.registro(authCredencialesDto);
+  }
 }
